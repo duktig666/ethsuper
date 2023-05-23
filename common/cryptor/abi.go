74749cf6cf3e1726b4a646d53758f6a496fc9d9a
RenShiWei <ren_shi_wei@qq.com>
2023-05-23 20:02:59 +0800

    Copyright

--- common/cryptor/abi.go
+++ common/cryptor/abi.go
@@ -1,3 +1,18 @@
+/*
+ * // Copyright © 2022-2023. duktig666 Limited.
+ * // Licensed under the Apache License, Version 2.0 (the "License");
+ * // you may not use this file except in compliance with the License.
+ * // You may obtain a copy of the License at
+ * //
+ * //     http://www.apache.org/licenses/LICENSE-2.0
+ * //
+ * // Unless required by applicable law or agreed to in writing, software
+ * // distributed under the License is distributed on an "AS IS" BASIS,
+ * // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * // See the License for the specific language governing permissions and
+ * // limitations under the License.
+ */
+
 // description: abi编码 @see https://github.com/0xsequence/ethkit/tree/master/ethcoder
 // @author renshiwei
 // Date: 2022/11/3 17:08
